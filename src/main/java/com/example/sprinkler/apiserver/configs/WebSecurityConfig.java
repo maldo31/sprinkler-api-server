6715c95c122921f4c5f47f09291e10cd17a60ece
maldo <maldok31@gmail.com>
2023-01-17 19:32:54 +0100

    minor fixes

--- src/main/java/com/example/sprinkler/apiserver/configs/WebSecurityConfig.java
+++ src/main/java/com/example/sprinkler/apiserver/configs/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.sprinkler.apiserver.configs;
 
-import java.util.Arrays;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
