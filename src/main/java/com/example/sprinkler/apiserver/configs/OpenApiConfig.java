a53d91d99b55f8c13c17a8c4a39c90115a3326f1
maldo <maldok31@gmail.com>
2023-02-05 21:36:19 +0100

    Added title and description to Swagger config

--- src/main/java/com/example/sprinkler/apiserver/configs/OpenApiConfig.java
+++ src/main/java/com/example/sprinkler/apiserver/configs/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.example.sprinkler.apiserver.configs;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,9 @@ public class OpenApiConfig {
     OpenAPI customizeOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .info(new Info().title("Sprinkler Server API")
+                        .title("REST API of sprinkler management server")
+                        .version("v1.0.0"))
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
                 .components(new Components()
